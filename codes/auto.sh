d64281486b6228a8dda8ad2ec5db773a6aeaf350
b06902033 <b06902033@csie.ntu.edu.tw>
2020-06-20 19:02:39 +0800

    changes over quarintine probabilty

--- codes/auto.sh
+++ codes/auto.sh
@@ -1,6 +1,11 @@
 #! /usr/bin/env bash
 for i in {1..11}; do
-	echo "python Model.py -dir cmp_days -day 10 -f Prob_02_${i} --no-ani"
-	python Model.py -dir cmp_days -day 10 -f Prob_02_${i} --no-ani
+	if [[ $i -eq 1 ]]; then
+		echo "python Model.py -dir cmp_days -day 10 -f Prob_02_${i}"
+		python Model.py -dir cmp_days -day 10 -f Prob_02_${i}
+	else
+		echo "python Model.py -dir cmp_days -day 10 -f Prob_02_${i} --no-ani"
+		python Model.py -dir cmp_days -day 10 -f Prob_02_${i} --no-ani
+	fi
 done
 
