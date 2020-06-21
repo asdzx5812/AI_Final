58bfb9bdd63d5013df22da6f4d11b2611dacab19
weiwe <b06705059@ntu.edu.tw>
2020-06-21 14:47:32 +0800

    shell script to mask problem

--- codes/auto_mask.sh
+++ codes/auto_mask.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/env bash
-pr=(0.292 0.915)
+p=(0.292 0.915)
 q=(0 1)
 m=(0.3 0.6 0.8 0.9)
 
 
 
-for j in ${pr[@]}; do
+for j in ${p[@]}; do
 	for k in ${q[@]}; do
 		for l in ${m[@]}; do
 
-			mkdir ../outputs/pr$j_q$k_w$l
+			mkdir ../outputs/"pr$j""_q$k""_w$l"
 			old_q=`cat SEIR_Model.py | sed -n "10p"`
 			old_p=`cat SEIR_Model.py | sed -n "11p"`
 			old_m=`cat SEIR_Model.py | sed -n "12p"`
@@ -34,8 +34,8 @@ for j in ${pr[@]}; do
 			for i in {1..5}; do
 				
 				
-				echo "python Model.py -day 10 -n 3000 -i 1 -dir pr$j_q$k_w$l -f $i --no-ani"
-				python Model.py -day 10 -n 3000 -i 1 -dir pr$j_q$k_w$l -f $i --no-ani &
+				echo "python Model.py -day 10 -n 3000 -i 1 -dir "pr$j""_q$k""_w$l" -f $i --no-ani"
+				python Model.py -day 10 -n 3000 -i 1 -dir "pr$j""_q$k""_w$l" -f $i --no-ani &
 
 	#python Model.py -day 10 -n 3000 -i 1 -dir $1 -f $i --no-ani &
 
